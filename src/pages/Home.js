3fc10d4597b9301299404759f258d7f5b8b564c2
allysonsmith <allysonsmithstudio@gmail.com>
2024-02-03 17:59:09 -0500

    Added footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 import {Cloudinary} from "@cloudinary/url-gen";
 import {AdvancedImage} from '@cloudinary/react';
 import {fill} from "@cloudinary/url-gen/actions/resize";
@@ -7,7 +8,7 @@ import {fill} from "@cloudinary/url-gen/actions/resize";
  const Home = () => {
     const cld = new Cloudinary({cloud: {cloudName: 'smith-studio'}});
   
-    const myImage = cld.image('allysonsmith/art/Purple-2024'); 
+    const myImage = cld.image('allysonsmith/art/Purple-ltst-2024'); 
     // Resize to 250 x 250 pixels using the 'fill' crop mode.
     myImage.resize(fill().width(1200));
 
@@ -15,6 +16,7 @@ import {fill} from "@cloudinary/url-gen/actions/resize";
      <div className="main">
          <Navbar />
          <AdvancedImage cldImg={myImage} className="image hero-image"/>
+         <Footer />
      </div>
    )
  }
