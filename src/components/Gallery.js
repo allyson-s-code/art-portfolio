18b341fd8a057a3fb1eb1035ca85436ba63c6b5e
allysonsmith <allysonsmithstudio@gmail.com>
2024-02-05 16:29:42 -0500

    Cloudinary product gallery works but without displaying metadata

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,6 +1,6 @@
 /*import { React, useEffect, useRef } from 'react';*/
 import React from 'react';
-import {Cloudinary} from "@cloudinary/url-gen";
+/*import {Cloudinary} from "@cloudinary/url-gen";*/
 
 const Gallery = () => {
    /*const containerRef = useRef(null);
@@ -16,20 +16,26 @@ const Gallery = () => {
           })
           .render();
         }
-      }, []); */
+      }, []); 
+    */
+    const myGallery = cloudinary.galleryWidget({
+        "container": "#gallery",
+        "cloudName": "smith-studio",
+        "mediaAssets": [{
+            "tag": "new-work",
+            "mediaType": "image"
+        }],
+        "aspectRatio": "3:4",
+        "zoomProps": {
+            "level": 1
+        },
+        "carouselStyle": "none"
+     });
+    myGallery.render();
       
-
-      const myGallery = cloudinary.galleryWidget({ 
-        container: "#gallery", 
-        cloudName: "smith-studio", 
-        mediaAssets: [{ tag: "2024" }]    // by default mediaType: "image"
-      });
-
-      myGallery.render();
-
       return <div id="gallery" style={{ width: '100%'}}></div>
     
-
+     
 }
 
 export default Gallery;
--- src/pages/Work.js
+++ src/pages/Work.js
@@ -15,3 +15,17 @@ import Footer from '../components/Footer'
  }
 
  export default Work
+
+ export async function getStaticProps() {
+    const results = await fetch(`https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/resources/image`, {
+        headers: {
+            Authorization: `Basic ${Buffer.from(process.env.CLOUDINARY_API_KEY + ':' + process.env.CLOUDINARY_API_SECRET).toString('base64')}`
+        }
+    }).then(r => r.json());
+    console.log('results', results)
+    return {
+        props: {
+
+        }
+    }
+ }
