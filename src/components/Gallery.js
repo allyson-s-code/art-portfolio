a7947c0d91efb00a551ed0970fb98fbf77a04c48
allysonsmith <allysonsmithstudio@gmail.com>
2024-02-06 17:37:23 -0500

    Switched to React Slick for slider component and having issues with styling

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,16 +1,40 @@
 import React from 'react';
+import data from '../Data.json';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 
 const Gallery = () => {
-   
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      };
+      console.log(data);
       return (
-      
-      <div id="gallery" style={{ width: '100%'}}>
-
-      </div>
+        <div className="slider-wrapper">
+            <Slider {...settings}>
+                {data.map((d) => (
+                    <div className="slide" key={d.id}>
+                        <div className="img-wrapper">
+                            <img src={d.url} alt={d.description} className="img" />
+                        </div>
+                        <div className="info-wrapper">
+                            <p className="title">{d.title}</p>
+                            <p>{d.medium}</p>
+                            <p>{d.dimensions}</p>
+                            <p>{d.date}</p>
+                        </div>
+                </div>
+                ))}
+            
+            </Slider>
+        </div>
+       
       )
-    
-     
 }
 
 export default Gallery;
