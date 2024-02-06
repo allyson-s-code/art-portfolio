c6b29ebd7415fc618f897a6662c8ce2f286926ce
allysonsmith <allysonsmithstudio@gmail.com>
2024-02-06 12:17:19 -0500

    Added react image gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,39 +1,14 @@
-/*import { React, useEffect, useRef } from 'react';*/
 import React from 'react';
-/*import {Cloudinary} from "@cloudinary/url-gen";*/
+
 
 const Gallery = () => {
-   /*const containerRef = useRef(null);
-
-    useEffect(() => {
-        if (window && containerRef.current) {
-          window.cloudinary.galleryWidget({
-            container: '#gallery',
-            cloudName: 'smith-studio',
-            mediaAssets: [
-                { tag: '2024'}
-            ],
-          })
-          .render();
-        }
-      }, []); 
-    */
-    const myGallery = cloudinary.galleryWidget({
-        "container": "#gallery",
-        "cloudName": "smith-studio",
-        "mediaAssets": [{
-            "tag": "new-work",
-            "mediaType": "image"
-        }],
-        "aspectRatio": "3:4",
-        "zoomProps": {
-            "level": 1
-        },
-        "carouselStyle": "none"
-     });
-    myGallery.render();
+   
+      return (
       
-      return <div id="gallery" style={{ width: '100%'}}></div>
+      <div id="gallery" style={{ width: '100%'}}>
+
+      </div>
+      )
     
      
 }
