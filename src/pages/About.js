093c753e1e5585f213fc4f4a8c32562bc5188e50
allysonsmith <allysonsmithstudio@gmail.com>
2024-02-09 08:56:52 -0500

    About page styling and playing with margins

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
  const About = () => {
    return (
      <div className="main">
          <Navbar />
-         <h1>About</h1>
+         <div className="about-info">
+            <p>Allyson Smith lives and works in Brooklyn, NY.</p>
+            <p>Feel free to reach out with any inquiries.</p>
+            <p>allysons{/* Comment */}mithstudio@gmail{/*-- comment --*/}.com</p>
+        </div>
+        <Footer />
      </div>
    )
  }
