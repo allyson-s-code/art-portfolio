c7100bcbf509670fa7411d486dc4e4b6b14f4212
allysonsmith <allysonsmithstudio@gmail.com>
2024-02-20 10:18:34 -0500

    Disabled class now toggles on smaller screen sizes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,105 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import HamburgerMenu from './HamburgerMenu';
 import { FaInstagram } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 
-
-
-/*const Navbar = () => {
-    const [isOpen, setIsOpen] = useState(false);
-
-    
-    
-      //when links are clicked I want to remove show class on nav because otherwise the menu doesn't close if link to same page is clicked
-    /*const links = document.querySelectorAll(".nav-links a")
-    
-    links.forEach(l => {
-        l.addEventListener('click', () => {
-            navBar.classList.remove('fadeIn');
-            setIsOpen(false);
-            navBar.classList.add('fadeOut');
-            setTimeout(() => {
-                navBar.classList.remove('show');
-            }, 1000);
-        })
-    })*/
-
-    /*const navBar = document.querySelector(".nav-links")
-
-    const handleOpenMenu = () => {
-        setIsOpen(true); 
-        navBar.classList.add('fadeIn');
-        setTimeout(() => {
-            navBar.classList.add('show');
-        }, 500); // this is to compensate for display transition happening immediately
-    }
- 
-    const handleCloseMenu = () => {
-        setIsOpen(false); 
-        // Trigger the fade-out effect before removing the "show" class
-        navBar.classList.remove('fadeIn');
-        navBar.classList.add('fadeOut');
-        setTimeout(() => {
-            navBar.classList.remove('show');
-        }, 500); // this is to compensate for display transition happening immediately
-    };
-
-
-    const handleToggle = () => {
-        setIsOpen(!isOpen);
-        return isOpen ? handleOpenMenu : handleCloseMenu;
-      };
-
-    return (
-    
-    <nav>
-      <Link to="/" className="site-title">Allyson Smith</Link>
-      <ul className="nav-links">
-        <li>
-            <Link to="/" onClick={handleCloseMenu}>Home</Link>
-        </li>
-        <li>
-            <Link to="/work" onClick={handleCloseMenu}>Work</Link>
-        </li>
-        <li>
-            <Link to="/about" onClick={handleCloseMenu}>About</Link>
-        </li>
-        <li>
-            <a href="https://www.instagram.com/allysonsmithstudio" target="_blank" rel="noreferrer" className="insta-icon" onClick={handleCloseMenu}>
-                <FaInstagram />
-            </a>
-        </li>
-      </ul>
-      < HamburgerMenu handleClick={handleToggle} isOpen={isOpen}/>
-    </nav>
-  );
-};
-*/
-
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
-    const [isAnimating, setIsAnimating] = useState(false);
+    const [isDisabled, setIsDisabled] = useState(false);
 
     const handleToggle = () => {
-        setIsAnimating(true); // Start animation
-        setTimeout(() => {
-            setIsOpen(!isOpen); // Toggle isOpen after animation starts
-            setIsAnimating(false); // Stop animation
-        }, 400); // Adjust this timeout to match your animation duration
+        setIsDisabled(!isDisabled);
+        setIsOpen(!isOpen); // Toggle isOpen after animation starts
     };
 
     const handleCloseMenu = () => {
-        setIsAnimating(true); // Start animation
         setTimeout(() => {
-            setIsAnimating(false);
-            setIsOpen(false); // Close menu after animation starts
-        }, 499); // Adjust this timeout to match your animation duration
+            setIsOpen(false);
+            if (window.innerWidth <= 769) {
+                setIsDisabled(true);
+            } // Close menu after fadeOut and disable links only on mobile nav
+        }, 300)
     };
+
+    useEffect(() => {
+        const handleResize = () => {
+            const largerScreen = window.innerWidth > 769;
+            setIsDisabled(!largerScreen);
+        };
+
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
 
     return (
         <nav>
             <Link to="/" className="site-title">Allyson Smith</Link>
-            <ul className={`nav-links ${isOpen ? 'fadeIn' : 'fadeOut'}`}>
+            <ul className={`nav-links ${isOpen ? 'fadeIn' : 'fadeOut'} ${isDisabled ? 'disabled' : ''}`}>
                 <li>
                     <Link to="/" onClick={handleCloseMenu}>Home</Link>
                 </li>
@@ -121,3 +59,65 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+
+/*const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [isDisabled, setIsDisabled] = useState(false);
+    const [isLargerScreen, setIsLargerScreen] = useState(window.innerWidth > 769);
+
+    const handleToggle = () => {
+        setTimeout(() => {
+            setIsDisabled(!isDisabled);
+            setIsOpen(!isOpen); // Toggle isOpen after animation starts
+        }, 400); // Adjust this timeout to match your animation duration
+    };
+
+    const handleCloseMenu = () => {
+        setTimeout(() => {
+            setIsOpen(false);
+            setIsDisabled(true)
+             // Close menu after fadeOut
+        }, 499); // Adjust this timeout to match your animation duration
+    };
+
+  
+    //remove disabled class on larger screens so nav bar works
+    useEffect(() => {
+        window.addEventListener("resize", () => {
+            const largerScreen = window.innerWidth > 769;
+            if (largerScreen !== isLargerScreen) {
+                setIsDisabled(false);
+            }
+        })
+    }, [isLargerScreen])
+
+    
+
+    return (
+        <nav>
+            <Link to="/" className="site-title">Allyson Smith</Link>
+            <ul className={`nav-links ${isOpen ? 'fadeIn' : 'fadeOut'} ${isDisabled ? 'disabled' : ''}`}>
+                <li>
+                    <Link to="/" onClick={handleCloseMenu}>Home</Link>
+                </li>
+                <li>
+                    <Link to="/work" onClick={handleCloseMenu}>Work</Link>
+                </li>
+                <li>
+                    <Link to="/about" onClick={handleCloseMenu}>About</Link>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/allysonsmithstudio" target="_blank" rel="noreferrer" className="insta-icon">
+                        <FaInstagram />
+                    </a>
+                </li>
+            </ul>
+            <HamburgerMenu handleClick={handleToggle} isOpen={isOpen} />
+        </nav>
+    );
+};
+
+
+export default Navbar;
+*/
